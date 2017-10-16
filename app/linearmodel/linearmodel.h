2dd8e0a3365f31df982581f2570413fad2b84c27
zeke <zeke@zeke-Precision-Tower-3620>
2017-10-16 15:56:53 +0200

    initial commit with sgd-icc-short

---
+++ app/linearmodel/linearmodel.h
@@ -0,0 +1,344 @@
+#ifndef _LINEARMODEL_H
+#define _LINEARMODEL_H
+
+#include "hazy/vector/fvector.h"
+#include "hazy/vector/svector.h"
+#include "hazy/vector/operations-inl.h"
+
+#ifdef AVX2_EN
+#include "hazy/vector/scale_add-inl_avx2.h"
+#else
+#include "hazy/vector/scale_add-inl.h"
+#endif
+
+
+struct LinearModel
+{
+  hazy::vector::FVector<fp_type> weights;
+  hazy::vector::FVector<fp_type> gradient;
+  hazy::vector::FVector<fp_type> * local_gradients;
+
+  float batch_step_size;    //!< current batch step size
+  DECREASING_STEPSIZES_ONLY(unsigned long long k;); //!< number of processed element for batch_step_size calculation
+
+  size_t nthreads_;
+
+  explicit LinearModel(unsigned dim, unsigned nthreads)
+  {
+    nthreads_ = nthreads;
+    weights.values = new fp_type[dim];
+    gradient.values = new fp_type[dim];
+    weights.size = dim;
+    gradient.size = dim;
+
+    for(unsigned i = 0; i < dim; ++i)
+    {
+      weights.values[i] = 0;
+      gradient.values[i] = 0;
+    }
+
+    if(nthreads > 1)
+    {
+      local_gradients = new hazy::vector::FVector<fp_type>[nthreads];
+    }
+    else
+    {
+      local_gradients = &gradient;
+    }
+  }
+
+  void initLocalVars(unsigned dim, unsigned tid)
+  {
+    local_gradients[tid].values = (fp_type*)aligned_alloc(CACHE_LINE_SIZE, dim * sizeof(fp_type));
+    local_gradients[tid].size = dim;
+    for (unsigned i = 0; i < dim; ++i) {
+      local_gradients[tid].values[i] = 0;
+    }
+  }
+
+  ~LinearModel()
+  {
+    if(nthreads_ > 1)
+    {
+      for(size_t j = 0; j < nthreads_; j++) {
+        free(local_gradients[j].values);
+      }
+      delete[] local_gradients;
+    }
+    delete[] weights.values;
+    delete[] gradient.values;
+  }
+};
+
+struct LinearModelParams
+{
+  unsigned class_model;       //first bit: enable binary classification, second bit: -1 or 0  Default: 0"
+  unsigned target_label;      //Target label to be identified. default:1
+  
+  unsigned batch_size;        //!< batch size
+  float step_size;            //!< stepsize (decayed by step decay at each epoch and/or after each mini batch...)
+  float step_decay;           //!< factor to modify step_size by each epoch
+  float beta;                 //!< the exponent constant for the stepsizes
+  float lasso_regularizer;    //!< lasso regularizer hyperparamter
+  uint64_t numSamplesProc;    //!< Number of samples on current worker
+  uint64_t totalNumSamples;   //!< Total number of samples
+  uint64_t maxSamplesProc;    //!< Max number on any worker
+  unsigned ndim;              //!< number of features, length of degrees
+  unsigned nthreads;          //!< Number of threads on working processes
+  unsigned quantizationLevel; //!< Quantization level (only affects LinReg and if quantization is > 0)
+  unsigned quantization;      //!< 0: No quantization / 1: Quatize samples / 2: quantize gradients / 3: quantize samples and gradient / 4: Quantize all Model / Sample and Gradient)
+  hazy::vector::FVector<fp_type> *x_hat;
+
+  LinearModelParams(fp_type stepsize, fp_type stepdecay) : step_size(stepsize), step_decay(stepdecay) { }
+};
+
+
+//////////////////////////////////////////char//////////////////////////////////////////////////////////
+struct LinearModelSample_char
+{
+  fp_type value;            //!< rating of this example
+  SPARSE_ONLY(hazy::vector::SVector<unsigned char> vector;); //!< feature vector
+  DENSE_ONLY( hazy::vector::FVector<unsigned char> vector;); //!< feature vector
+
+  LinearModelSample_char()
+  { }
+
+#ifdef _SPARSE
+  LinearModelSample_char(
+    fp_type val,
+    unsigned char  *values,
+    int *index, 
+    unsigned len,
+    int dimension
+  ) : value(val), vector(values, index, len) { }
+#else
+  LinearModelSample_char(
+    fp_type val,
+    unsigned char  *values,
+    int *index, 
+    unsigned len,
+    int dimension
+  ) : value(val) {
+
+    hazy::vector::SVector<unsigned char> sparse_vector(values, index, len);
+
+    unsigned char *zeros = new unsigned char[dimension];
+    hazy::vector::FVector<unsigned char> *temp_vector = new hazy::vector::FVector<unsigned char>(zeros, dimension);
+
+    // set to zero
+    hazy::vector::Zero(*temp_vector);
+    
+    hazy::vector::ScaleAndAdd(
+		      *temp_vector,
+		      sparse_vector,
+		      1.0
+    );
+
+    vector.size = (*temp_vector).size;
+    vector.values = (*temp_vector).values;
+  }
+#endif
+
+  LinearModelSample_char(const LinearModelSample_char &o) {
+    value = o.value;
+    vector.values = o.vector.values;
+    SPARSE_ONLY(vector.index = o.vector.index;);
+    vector.size = o.vector.size;
+  }
+
+  float b_binary_to_value()
+  {
+  	return 256.0;
+  }
+  void exchange_sample(LinearModelSample_char &exchange)
+  {
+    //exhange the values.
+    unsigned char* this_vector = (unsigned char*)(this->vector.values);
+    unsigned char* exch_vector = (unsigned char*)exchange.vector.values;
+    unsigned this_size   = this->vector.size;
+    unsigned exch_size   = exchange.vector.size;
+    unsigned char temp[this_size];
+    memcpy(         temp,            (void *) this_vector,   this_size * sizeof(char));
+    memcpy((void *) this_vector,     (void *) exch_vector,   this_size * sizeof(char));
+    memcpy((void *) exch_vector,          temp,              this_size * sizeof(char));
+
+     //exhange the label.
+    fp_type label_tmp;
+    label_tmp        = this->value;
+    this->value      = exchange.value;      
+    exchange.value   = label_tmp;    
+  }
+
+};
+
+
+
+
+
+//////////////////////////////////////////short//////////////////////////////////////////////////////////
+struct LinearModelSample_short
+{
+  fp_type value;            //!< rating of this example
+  SPARSE_ONLY(hazy::vector::SVector<unsigned short> vector;); //!< feature vector
+  DENSE_ONLY( hazy::vector::FVector<unsigned short> vector;); //!< feature vector
+
+  LinearModelSample_short()
+  { }
+
+#ifdef _SPARSE
+  LinearModelSample_short(
+    fp_type val,
+    unsigned short  *values,
+    int *index, 
+    unsigned len,
+    int dimension
+  ) : value(val), vector(values, index, len) { }
+#else
+  LinearModelSample_short(
+    fp_type val,
+    unsigned short  *values,
+    int *index, 
+    unsigned len,
+    int dimension
+  ) : value(val) {
+
+    hazy::vector::SVector<unsigned short> sparse_vector(values, index, len);
+
+    unsigned short *zeros = new unsigned short[dimension];
+    hazy::vector::FVector<unsigned short> *temp_vector = new hazy::vector::FVector<unsigned short>(zeros, dimension);
+
+    // set to zero
+    hazy::vector::Zero(*temp_vector);
+    
+    hazy::vector::ScaleAndAdd(
+		      *temp_vector,
+		      sparse_vector,
+		      1.0
+    );
+
+    vector.size = (*temp_vector).size;
+    vector.values = (*temp_vector).values;
+  }
+#endif
+
+  LinearModelSample_short(const LinearModelSample_short &o) {
+    value = o.value;
+    vector.values = o.vector.values;
+    SPARSE_ONLY(vector.index = o.vector.index;);
+    vector.size = o.vector.size;
+  }
+
+  float b_binary_to_value()
+  {
+  	return 65536.0;
+  }
+  void exchange_sample(LinearModelSample_short &exchange)
+  {
+    //exhange the values.
+    unsigned short* this_vector = (unsigned short*)(this->vector.values);
+    unsigned short* exch_vector = (unsigned short*)exchange.vector.values;
+    unsigned this_size   = this->vector.size;
+    unsigned exch_size   = exchange.vector.size;
+    short temp[this_size];
+    memcpy(         temp,            (void *) this_vector,   this_size * sizeof(short));
+    memcpy((void *) this_vector,     (void *) exch_vector,   this_size * sizeof(short));
+    memcpy((void *) exch_vector,          temp,              this_size * sizeof(short));
+
+     //exhange the label.
+    fp_type label_tmp;
+    label_tmp        = this->value;
+    this->value      = exchange.value;      
+    exchange.value   = label_tmp;    
+  }
+
+};
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////float//////////////////////////////////////////////////////////
+struct LinearModelSample
+{
+  fp_type value;            //!< rating of this example
+  SPARSE_ONLY(hazy::vector::SVector<const fp_type> vector;); //!< feature vector
+  DENSE_ONLY( hazy::vector::FVector<const fp_type> vector;); //!< feature vector
+
+  LinearModelSample()
+  { }
+
+#ifdef _SPARSE
+  LinearModelSample(
+    fp_type val,
+    fp_type const *values,
+    int *index, 
+    unsigned len,
+    int dimension
+  ) : value(val), vector(values, index, len) { }
+#else
+  LinearModelSample(
+    fp_type val,
+    fp_type const *values,
+    int *index, 
+    unsigned len,
+    int dimension
+  ) : value(val) {
+
+    hazy::vector::SVector<const fp_type> sparse_vector(values, index, len);
+
+    fp_type *zeros = new fp_type[dimension];
+    hazy::vector::FVector<fp_type> *temp_vector = new hazy::vector::FVector<fp_type>(zeros, dimension);
+
+    // set to zero
+    hazy::vector::Zero(*temp_vector);
+    
+    hazy::vector::ScaleAndAdd(
+		      *temp_vector,
+		      sparse_vector,
+		      1.0
+    );
+
+    vector.size = (*temp_vector).size;
+    vector.values = (*temp_vector).values;
+  }
+#endif
+
+  LinearModelSample(const LinearModelSample &o) {
+    value = o.value;
+    vector.values = o.vector.values;
+    SPARSE_ONLY(vector.index = o.vector.index;);
+    vector.size = o.vector.size;
+  }
+
+  float b_binary_to_value()
+  {
+  	return 1.0;
+  }
+
+  void exchange_sample(LinearModelSample &exchange)
+  {
+    //exhange the values.
+    fp_type* this_vector = (fp_type*)(this->vector.values);
+    fp_type* exch_vector = (fp_type*)exchange.vector.values;
+    unsigned this_size   = this->vector.size;
+    unsigned exch_size   = exchange.vector.size;
+    fp_type temp[this_size];
+    memcpy(         temp,            (void *) this_vector,   this_size * sizeof(fp_type));
+    memcpy((void *) this_vector,     (void *) exch_vector,   this_size * sizeof(fp_type));
+    memcpy((void *) exch_vector,          temp,              this_size * sizeof(fp_type));
+
+     //exhange the label.
+    fp_type label_tmp;
+    label_tmp        = this->value;
+    this->value      = exchange.value;      
+    exchange.value   = label_tmp;    
+  }
+
+};
+
+#endif
---
+++ src/strategy/hogwild.h
@@ -0,0 +1,486 @@
+#ifndef _HOGWILD_H
+#define _HOGWILD_H
+
+#include "global_macros.h"
+
+#ifndef _NUMA_INIT
+#define _NUMA_INIT
+#define CPUS_PER_NODE 10 // 10: Withouth HT / 20: With HT
+#define NUMA_NODES 4
+#endif
+
+#include "hazy/scan/binfscan.h"
+#include "hazy/scan/tsvfscan.h"
+#include "hazy/scan/memscan.h"
+#include "hazy/scan/sampleblock.h"
+
+#include "hazy/vector/operations-inl.h"
+
+#ifdef AVX2_EN
+#include "hazy/vector/scale_add-inl_avx2.h"
+#else
+#include "hazy/vector/scale_add-inl.h"
+#endif
+
+#include "hazy/thread/thread_pool-inl.h"
+#include "hazy/util/clock.h"
+#include "hazy/util/simple_random-inl.h"
+
+#include "utils.h"
+
+#include "types/thread_args.h"
+#include "types/aligned_pointer.h"
+#include "types/timers_info.h"
+
+#include <stddef.h> 
+
+namespace __executor
+{
+  template< class Model, class Params, class Sample, class Exec > void ComputeLossPerThread(ThreadArgs<Model, Params, Sample> &threadArgs, unsigned tid, unsigned total)
+  {
+    Model *model = threadArgs.model_;
+    //Params const &params = *threadArgs.params_;
+    size_t *current_batch = threadArgs.current_batch_;
+    size_t numElems = threadArgs.actual_num_elements_in_batch;
+
+    hazy::vector::FVector<Sample> const & sampsvec = threadArgs.block_->ex;
+
+    // calculate which chunk of examples we work on
+    size_t start = GetStartIndex(numElems, tid, total); 
+    size_t end = GetEndIndex(numElems, tid, total);
+
+    if((end - start) == 0)
+      return; // DO NOTHING ON THIS THREAD
+
+    // keep const correctness
+    Sample const * const samps = sampsvec.values;
+    fp_type loss = 0.0;
+    // compute the loss for each example
+    for (unsigned i = start; i < end; i++)
+    {
+      /* use this commented function to calculate statistics of x_hat, also uncomment params */
+      //fp_type l = Exec::ComputeMetaLoss(samps[i], params);
+
+      fp_type l = Exec::SingleLoss(samps[*current_batch + i], model); // Ignore permutation
+      loss += l;
+    }
+
+    *threadArgs.losses_[tid].ptr = loss;
+  }
+
+  template< class Model, class Params, class Sample, class Exec > void RunHogwildPerThread(ThreadArgs<Model, Params, Sample> &threadArgs, unsigned tid, unsigned total)
+  {
+    Model *model = threadArgs.model_;
+    Params const &params = *threadArgs.params_;
+    hazy::vector::FVector<Sample> const & sampsvec = threadArgs.block_->ex;
+    Sample * samps = sampsvec.values;
+    size_t *perm = threadArgs.block_->perm.values;
+
+	        threadArgs.compute_times_[tid].ptr->Start();//wzk: pure computation time...
+
+    // calculate which chunk of examples we work on
+    size_t start = GetStartIndex(sampsvec.size, tid, total); 
+    size_t end = GetEndIndex(sampsvec.size, tid, total);
+
+    size_t batch_size = params.batch_size;
+    size_t * current_batch = new size_t[batch_size];
+    size_t actual_num_elements_in_batch = 0;
+
+	float b_base = samps[0].b_binary_to_value();  //65536.0; //1.0; // //2^16 or  1
+	//samps
+    model->batch_step_size = params.step_size/((float)batch_size*b_base*b_base); 
+
+    for (unsigned i = start; i < end; i++) {
+      size_t indirect = perm[i];
+      current_batch[i % batch_size] = indirect;
+
+      if((i - start) % batch_size == batch_size - 1 || i == end - 1)
+      {
+        DECREASING_STEPSIZES_ONLY(model->batch_step_size *= pow(model->k, -params.beta));
+
+        // Reset gradient
+        hazy::vector::Zero(model->local_gradients[tid]);
+
+        actual_num_elements_in_batch = ((i - start) % batch_size) + 1;
+
+        //
+        Exec::CalcModelUpdate( samps, current_batch, actual_num_elements_in_batch, model, params, tid);
+        //
+
+        //threadArgs.communicate_times_[tid].ptr->Start();
+        hazy::vector::ScaleAndAdd(
+            model->weights,
+            model->local_gradients[tid],
+            1.0
+            );
+        //threadArgs.communicate_times_[tid].ptr->Pause();
+
+        DECREASING_STEPSIZES_ONLY(model->k += actual_num_elements_in_batch);
+      }
+    }  
+
+	       threadArgs.compute_times_[tid].ptr->Pause();//wzk: pure computation time...
+
+    delete[] current_batch;
+  }
+
+  template< class Model, class Params, class Sample, class Exec > void InitPerThread(ThreadArgs<Model, Params, Sample> &threadArgs, unsigned tid, unsigned total)
+  {
+    Params const &params = *threadArgs.params_;
+    Model *model = threadArgs.model_;
+
+    // init in a way that each thread will allocate its own memory
+    model->initLocalVars(params.ndim, tid);
+    threadArgs.losses_[tid].ptr = new double;
+    threadArgs.compute_times_[tid].ptr = new hazy::util::Clock;
+    threadArgs.communicate_times_[tid].ptr = new hazy::util::Clock;
+  }
+}
+
+template< class Model, class Params, class Sample, class Loader, class Exec>
+class Hogwild
+{
+  public:
+    Hogwild() : model_(NULL), params_(NULL), rank_(0)
+    {
+      wall_clock_.Start();
+    }
+
+    virtual ~Hogwild()
+    {
+      for(size_t i = 0; i < params_->nthreads; ++i)
+      {
+        delete losses_[i].ptr;
+        delete compute_times_[i].ptr;
+        delete communicate_times_[i].ptr;
+      }
+      free(losses_);
+      free(compute_times_);
+      free(communicate_times_);
+
+      threadPool_->Join();
+      delete threadPool_;
+      delete model_;
+      delete trainScan_;
+      delete testScan_;
+    }
+
+    double NormTwoXMinusXHat()
+    {
+      hazy::vector::FVector<fp_type> const &x = this->model_->weights;
+
+      fp_type*zeros = new fp_type[this->params_->ndim];
+      hazy::vector::FVector<fp_type> *diff = new hazy::vector::FVector<fp_type>(zeros, this->params_->ndim);
+
+      // set to zero
+      hazy::vector::Zero(*diff);
+
+      hazy::vector::ScaleAndAdd((*diff), x, 1.0);
+      hazy::vector::ScaleAndAdd((*diff), *this->params_->x_hat, -1.0);
+
+      double ret = hazy::vector::Norm2WithoutSquare((*diff));
+
+      delete[] zeros;
+      delete[] diff;
+      return ret;
+    }
+
+    template< class Scan > fp_type ComputeLoss(Scan *scan)
+    {
+      fp_type loss = 0.0;
+      int numSamples = 0;
+      scan->Reset();
+
+      test_time_.Start();
+      while(scan->HasNext())
+      {
+        hazy::scan::SampleBlock<Sample> &block = scan->Next();
+
+        // Set losses to 0
+        for(size_t i = 0; i < this->params_->nthreads; ++i)
+        {
+          *losses_[i].ptr = 0;
+        }
+
+        ThreadArgs<Model, Params, Sample> args;
+        args.model_ = this->model_;
+        args.params_ = this->params_;
+        args.rank_ = this->rank_;
+        args.losses_ = this->losses_;
+        args.block_ = &block;
+
+        size_t current_batch[1];
+        current_batch[0] = 0;
+
+        args.actual_num_elements_in_batch = block.ex.size;
+        args.current_batch_ = current_batch;
+        numSamples += block.ex.size;
+
+        // Run on thread pool
+        threadPool_->Execute(args, __executor::ComputeLossPerThread<Model, Params, Sample, Exec>);
+        threadPool_->Wait();
+
+        // Sum uf losses
+        for(size_t i = 0; i < this->params_->nthreads; ++i)
+        {
+          loss += *losses_[i].ptr;
+        }
+      }
+      test_time_.Stop();
+
+      if(numSamples == 0)
+        return 0.0;
+
+      return loss / numSamples;
+    }
+
+	//denormalization to binary classification...
+	template< class Sample > void b_normalize(hazy::vector::FVector<Sample> &samps)
+	{
+		unsigned class_model    = this->params_->class_model;	//	
+		unsigned target_label   = this->params_->target_label; //intger.
+		bool normalize_enable   = class_model&1;     //first bit..
+		bool toMinus1_1         = (class_model>>1)&1;//second bit..
+		float base              = samps[0].b_binary_to_value();
+			
+		float not_targeted      = toMinus1_1? (base*(-1.0)): 0.0;
+		unsigned num_samps      = samps.size;
+
+		//printf("base = %f, class_model = %d, normalize_enable = %d, toMinus1_1=%d, num_samps= %d\n", base, class_model, normalize_enable, toMinus1_1, num_samps );
+		
+		if (normalize_enable)
+		{
+			for (int i = 0; i < num_samps; i++)
+			{
+				//if(i < 10)
+					//printf("%d: original %f, ", i, samps[i].value);
+				if (samps[i].value != target_label) //not the target label, assign to -1 or 0. 
+					samps[i].value = not_targeted;
+				else
+					samps[i].value = base;//1.0;             //1.0 for the targeted label...
+				//if(i < 10)
+					//printf(" now %f", samps[i].value);
+			}
+		}
+		return;
+	}
+    template< class Scan > void RunEpoch(Scan *scan)
+    {
+      scan->Reset();
+
+      train_time_.Start();
+      epoch_time_.Start();
+
+      while(scan->HasNext())
+      {
+        hazy::scan::SampleBlock<Sample> &block = scan->Next();
+
+        ThreadArgs<Model, Params, Sample> args;
+        args.model_ = this->model_;
+        args.params_ = this->params_;
+        args.rank_ = this->rank_;
+        args.block_ = &block;
+        args.compute_times_ = this->compute_times_ ;
+        args.communicate_times_ = this->communicate_times_ ;
+
+        threadPool_->Execute(args, __executor::RunHogwildPerThread<Model, Params, Sample, Exec>);
+        threadPool_->Wait();
+      }
+
+      epoch_time_.Stop();
+      train_time_.Pause();
+    }
+
+    void Init(char *szTrainFile, char *szTestFile, char *szMetadataFile, bool loadBinary, bool matlab_tsv, int dimension, unsigned nthreads)
+    {
+      hazy::vector::FVector<Sample> train_samps;
+      hazy::vector::FVector<Sample> test_samps;
+      hazy::vector::FVector<Sample> metadata;
+
+      printDebug(rank_, "Loading training samples from '%s'", szTrainFile);
+
+      if (loadBinary) {
+        hazy::scan::BinaryFileScanner scan(szTrainFile);
+        Loader::LoadSamples(scan, train_samps, dimension);
+      } else if (matlab_tsv) {
+        hazy::scan::MatlabTSVFileScanner scan(szTrainFile);
+        Loader::LoadSamples(scan, train_samps, dimension);
+      } else {
+        hazy::scan::TSVFileScanner scan(szTrainFile);
+        Loader::LoadSamples(scan, train_samps, dimension);
+      }
+
+      printDebug(rank_, "Loading test samples from '%s'", szTestFile);
+      if (matlab_tsv) {
+        hazy::scan::MatlabTSVFileScanner scantest(szTestFile);
+        Loader::LoadSamples(scantest, test_samps, dimension);
+      } else {
+        hazy::scan::TSVFileScanner scantest(szTestFile);
+        Loader::LoadSamples(scantest, test_samps, dimension);
+      }
+
+      //hazy::scan::TSVFileScanner scan_metadata(szMetadataFile);
+      //Loader::LoadSamples(scan_metadata, metadata, dimension);
+
+      printDebug(rank_, "Loaded %lu training samples", train_samps.size);
+      printDebug(rank_, "Loaded %lu test samples", test_samps.size);
+
+      b_normalize(train_samps);
+      b_normalize(test_samps);
+      //printDebug(rank_, "After b normalization. \n");
+
+      params_->ndim = dimension;
+      params_->numSamplesProc = train_samps.size;
+      params_->nthreads = nthreads;
+
+      fp_type *d = new fp_type[dimension];
+      params_->x_hat= new hazy::vector::FVector<fp_type>(d, dimension);
+      hazy::vector::Zero(*params_->x_hat);
+      //hazy::vector::ScaleAndAdd(*params_->x_hat, metadata.values[0].vector, 1.0);
+
+      model_ = new Model(dimension, nthreads);
+
+      //trainScan_ = new hazy::scan::MemoryScan< Sample >(train_samps);
+      //testScan_ = new hazy::scan::MemoryScan< Sample >(test_samps);
+      //trainScan_ = new hazy::scan::MemoryScanNoPermutation< Sample >(train_samps);
+      //testScan_ = new hazy::scan::MemoryScanNoPermutation< Sample >(test_samps);
+      trainScan_  = new hazy::scan::MemoryScanPermuteValues< Sample >(train_samps);
+      testScan_   = new hazy::scan::MemoryScanPermuteValues< Sample >(test_samps);
+
+      // Get max number of samples on any worker
+      params_->maxSamplesProc = train_samps.size;
+      params_->totalNumSamples = train_samps.size;
+
+      if(this->params_->batch_size < 1)
+      {
+        // Vanilla gradient descent
+        this->params_->batch_size = this->params_->maxSamplesProc;
+      }
+
+      DECREASING_STEPSIZES_ONLY(model_->k = 1);
+      //DECREASING_STEPSIZES_ONLY(model_->k = params_->batch_size > 1 ? (params_->batch_size / 2) : 1);
+
+      // Init threadpool
+
+      //Cache aligned allocation
+      losses_ = (AlignedPointer<double>*)aligned_alloc(CACHE_LINE_SIZE, nthreads * sizeof(AlignedPointer<double>));
+      compute_times_ = (AlignedPointer<hazy::util::Clock>*)aligned_alloc(CACHE_LINE_SIZE, nthreads * sizeof(AlignedPointer<hazy::util::Clock>));
+      communicate_times_ = (AlignedPointer<hazy::util::Clock>*)aligned_alloc(CACHE_LINE_SIZE, nthreads * sizeof(AlignedPointer<hazy::util::Clock>));
+      printDebug(rank_, "Run threadpool with %u threads", nthreads);
+
+      threadPool_ = new hazy::thread::ThreadPool(nthreads);
+      threadPool_->Init();
+	  
+      //printDebug(rank_, "After threadPool_init\n");
+
+      // Init 
+      ThreadArgs<Model, Params, Sample> args;
+      args.model_ = this->model_;
+      args.params_ = this->params_;
+      args.losses_ = this->losses_;
+      args.compute_times_ = this->compute_times_ ;
+      args.communicate_times_ = this->communicate_times_ ;
+
+      threadPool_->Execute(args, __executor::InitPerThread<Model, Params, Sample, Exec>);
+     // printDebug(rank_, "After threadPool_Execute\n");
+
+      threadPool_->Wait();
+     // printDebug(rank_, "After threadPool_WAIT\n");	  
+    }
+
+    void Run(int nepochs)
+    {
+      //printDebug(rank_, "beginning In run, nepochs = %d\n", nepochs);	 
+	  
+      double totalTime = 0.0;
+	  double pure_total_computing_time = 0.0;
+	  printf("this->params_->step_size = %.9f\n", this->params_->step_size);
+	  
+      for(int e = 0; e <= nepochs; ++e)
+      {      
+        double avgComputeTime = 0.0;
+        double avgCommunicateTime = 0.0;
+        if(e > 0)
+        {
+          this->RunEpoch(trainScan_);
+
+          // Calc average and reset compute and communicate timers
+          for(unsigned i = 0; i < params_->nthreads; ++i)
+          {
+            avgComputeTime += compute_times_[i].ptr->value;
+            avgCommunicateTime += communicate_times_[i].ptr->value;
+            compute_times_[i].ptr->Reset();     // Force reset
+            communicate_times_[i].ptr->Reset(); // Force reset
+          }
+          if(params_->nthreads > 0)
+          {
+            avgComputeTime /= params_->nthreads;
+            avgCommunicateTime /= params_->nthreads;
+          }
+		  pure_total_computing_time += avgComputeTime;
+        }
+		
+#ifdef _EXPBACKOFF_STEPSIZES
+        this->params_->step_size *= this->params_->step_decay;
+#endif
+
+        double train_loss = this->ComputeLoss(trainScan_);
+        double test_loss = this->ComputeLoss(testScan_);
+
+        totalTime += epoch_time_.value;
+/*
+        double norm_x_minus_x_hat = this->NormTwoXMinusXHat();
+        printf("epoch: %.2d wall_clock: %.7f train_time: %.7f test_time: %.7f epoch_time: %.7f compute_time: %.7f communicate_time: %.7f train_loss: %.7f test_loss: %.7f norm_x_minus_x_hat: %.7f\n", 
+            e,
+            wall_clock_.Read(),
+            train_time_.value,
+            test_time_.value,
+            epoch_time_.value,
+            avgComputeTime,
+            avgCommunicateTime,
+            train_loss,
+            test_loss,
+            norm_x_minus_x_hat
+            );
+*/
+        printf("epoch: %.2d   train_time (total, each): with_thread_sync(%.7f, %.7f), without_thread_sync(%.7f, %.7f)  train_loss: %.7f test_loss: %.7f\n", //communicate_time: %.7f
+            e,
+            train_time_.value,
+            epoch_time_.value,
+            pure_total_computing_time,
+            avgComputeTime,
+            //avgCommunicateTime,
+            train_loss,
+            test_loss
+            );
+        fflush(stdout);
+      }
+
+      printf("%f\nFinished!\n", totalTime / nepochs);
+    }
+
+    Model *model_;
+    Params *params_;
+
+    hazy::thread::ThreadPool* threadPool_;
+    AlignedPointer<double>* losses_;
+    AlignedPointer<hazy::util::Clock>* compute_times_;
+    AlignedPointer<hazy::util::Clock>* communicate_times_;
+
+    hazy::util::Clock wall_clock_;
+    hazy::util::Clock train_time_;
+    hazy::util::Clock test_time_;
+    hazy::util::Clock epoch_time_;
+    hazy::util::Clock pure_train_time_;	
+
+    int rank_;
+
+    //hazy::scan::MemoryScan< Sample > *trainScan_;
+    //hazy::scan::MemoryScan< Sample > *testScan_;
+    //hazy::scan::MemoryScanNoPermutation< Sample > *trainScan_;
+    //hazy::scan::MemoryScanNoPermutation< Sample > *testScan_;
+    hazy::scan::MemoryScanPermuteValues< Sample > *trainScan_;
+    hazy::scan::MemoryScanPermuteValues< Sample > *testScan_;    
+};
+
+#endif
